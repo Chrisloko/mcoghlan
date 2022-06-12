3073f42ca58013d7238f90172543a54f2b7a9d55
Rodrigo <rodrigo.amansala@gmail.com>
2022-06-12 01:07:49 -0500

    cambios 4500

---
+++ contacto-prueba.php
@@ -0,0 +1,132 @@
+<?php 
+$title="Contacto Mariangel Coghlan";
+$desc = "Contáctanos, te ayudamos a diseñar tu casa, departamento, oficina, restaurante o hotel y que así sea única y exclusiva.";
+$key = "diseño de interiores";
+$page = 'contacto';
+$canonical = "https://www.mcoghlan.mx/contacto-mariangel-coghlan";
+include('global/header.php');
+?>	
+<style>
+	#rd-form-l400xhg3 .bricks-form__field:nth-child(5) .bricks-form__field__option .bricks-form__label {
+		display:flex!important;
+		align-items:center;
+	}
+	#rd-form-l400xhg3 .bricks-form__field:nth-child(5) .bricks-form__field__option .bricks-form__label input{
+		width: unset!important;
+		margin-right: 10px;
+	}
+	#rd-column-l400xhg2 > div{
+		padding-left: 0!important;
+		padding-right: 0!important;
+	}
+	#rd-column-l400xhg2 input[type=text]{
+		border: 1px solid #e3e3e3;
+		font-size: 1em;
+		border-radius: 4px;
+		border-width: 1px;
+		border-style: solid;
+		display: block;
+	}
+	#rd-column-l400xhg2 input[type=email] {
+		width: 100%;
+		padding: 12px 20px;
+		margin: 4px 0;
+		color: #000;
+		display: inline-block;
+		border: 1px solid #e3e3e3;
+		border-radius: 20px;
+		background-color: #fff;
+		box-sizing: border-box;
+		max-width: 100%;
+		border: 1px solid #ccc;
+		transition: border 0.2s linear,box-shadow 0.2s linear;
+		box-shadow: inset 0 1px 3px rgb(0 0 0 / 10%);
+	}
+	#rd-column-l400xhg2 select {
+		width: 100%!important;
+		padding: 7px 20px!important;
+		margin: 4px 0 15px!important;
+		color: #000;
+		display: inline-block!important;
+		border: 1px solid #e3e3e3!important;
+		border-radius: 20px!important;
+		background-color: #fff!important;
+		box-sizing: border-box!important;
+		max-width: 100%!important;
+		line-height: 18px;
+		/* color: #666; */
+		border: 1px solid #ccc;
+		transition: border 0.2s linear,box-shadow 0.2s linear;
+		box-shadow: inset 0 1px 3px rgb(0 0 0 / 10%);
+	}
+	#rd-button-l400xhg6 {
+		text-shadow: 0 1px 0 #1e549d!important;
+    	border: 1px solid #3d7fb1!important;
+		background: #3C4650!important;
+		background-color: #3C4650!important;
+	}
+</style>
+      <!-- #header-wrap -->
+ <section id="hero-interior" class="hero-contacto">
+ <div class="container">
+		<div class="row">
+			<div class="text-cont">
+				<h2>CONTACTO</h2>
+			</div>
+		</div>
+  </div>
+ </section>  
+ <section id="form-contacto">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+               <h3>Contacto</h3> 
+            </div>
+            <div class="col-12">
+				<div role="main" id="contacto-23b924cb8a8c362a50fd"></div><script type="text/javascript" src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"></script><script type="text/javascript"> new RDStationForms('contacto-23b924cb8a8c362a50fd', 'UA-38568777-1').createForm();</script>
+			</div>
+        </div>
+    </div>
+</section>
+ 
+<section id="datos-contacto">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 d-flex">
+				<div class="item-sm">
+					<img src="img/location.png" alt="Diseño de Interiores">
+				</div>
+				<div class="item-lg">
+					<a href="https://g.page/InteriorismoMariangelCoghlan?share">Av. Revolución 1495 San Ángel Ciudad de México, C.P. 01040</a>
+				</div>
+			</div>
+			<div class="col-md-4 d-flex datos">
+				<div class="item-sm">
+					<img src="img/phone.png" alt="Diseño de Interiores">
+				</div>
+				<div class="item-lg">
+					<a href="callto:+525556619385">(55) 5661 9385</a>
+					<a href="callto:+525556629789">(55) 5662 9789</a>
+				</div>
+			</div>
+			<div class="col-md-4 d-flex">
+				<div class="item-sm">
+					<img src="img/email-world.png" alt="Diseño de Interiores">
+				</div>
+				<div class="item-lg">
+					<a href="mailto:info@mcoghlan.mx">info@mcoghlan.mx</a>
+					<a href="https://www.mcoghlan.mx">www.mcoghlan.mx</a>
+				</div>
+			</div>
+			<div class="col-12">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1882.1823969430807!2d-99.190551!3d19.353351!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x42bb41506ee1f0e1!2sInteriorismo%20MARIANGEL%20COGHLAN!5e0!3m2!1sen!2smx!4v1624029029636!5m2!1sen!2smx" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include('global/cta-visita.php');?>	
+<?php 
+include('global/footer.php');
+?>	
+
+    
